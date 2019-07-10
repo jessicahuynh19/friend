1b6bfbec74700abece8d856fa593c0e72405de39
Jessica Huynh <huynhjessica19@gmail.com>
2019-07-09 23:05:51 -0700

    finished product

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Marshmello',
+		photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxKUhaaCCs2ciibqFE97b7hS6ko8OpXzBO6JZK9M40Hd2OVGlbkw',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Dillion Francis',
+		photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHeOre20yEWIBZaCkj-OD5WHG0a-cz9XW2j-OyKUhR8pcF1znt',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Steve Aoki',
+		photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMw8E5Cn2norU5zD3YlJFVbBBDAZM61CJjyOJRG9wdDdfzD236mA',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'David Dobrik',
+		photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-agwS1wjESDE6M7PIoM_3oGhmz9rxCSYMFKUfcJZT4KoLXT9sfQ',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Jason Nash',
+		photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTee5Lw65qt8z-7BoFySKYmQCM96MkdMAtoZs-W52NjAKpwcHna',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Eugene Lee Yang',
+		photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1drV4ihrXLacW_PXqf-TOYGCHbOwgD132hl6l3cj4QO_qw7Qbgw',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Zach Kornfeld',
+		photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQF706CMHJH22VRfM_OWnGCvDNQGX9RtLtNNvVq3eMAT95Dp-Uv',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Keith Habersberger',
+		photo: 'https://i1.imgiz.com/data/videoshots/6829/6829475-6461.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Ned Fulmer',
+		photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8IuWo6OKO-B25zEIZRGfhxviZGXW9BTJeSyUwSD9D51xpFaYPBA',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Pitbull',
+		photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_rJ5vbZgyLPmOt0teGR9bb0ZTcpFPk8tUciM0gE_9n2aetmca7Q',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
